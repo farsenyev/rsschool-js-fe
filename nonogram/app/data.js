5f5f79d6a41e49b985c4c5548b8aef672278d207
Ted Arsenev <farsenyev@bk.ru>
2024-01-27 22:43:08 +0300

    feat: add data and images

---
+++ nonogram/app/data.js
@@ -0,0 +1,64 @@
+const data = [
+    {
+        'path': './pixels/rune.png',
+        'difficulty': 'easy'
+    },
+    {
+        'path': './pixels/plus.png',
+        'difficulty': 'easy'
+    },
+    {
+        'path': './pixels/point.png',
+        'difficulty': 'easy'
+    },
+    {
+        'path': './pixels/tower.png',
+        'difficulty': 'easy'
+    },
+    {
+        'path': './pixels/skull.png',
+        'difficulty': 'easy'
+    },
+    {
+        'path': './pixels/cap.png',
+        'difficulty': 'medium'
+    },
+    {
+        'path': './pixels/clock.png',
+        'difficulty': 'medium'
+    },
+    {
+        'path': './pixels/infector.png',
+        'difficulty': 'medium'
+    },
+    {
+        'path': './pixels/leaf.png',
+        'difficulty': 'medium'
+    },
+    {
+        'path': './pixels/music.png',
+        'difficulty': 'medium'
+    },
+    {
+        'path': './pixels/tv.png',
+        'difficulty': 'medium'
+    },
+    {
+        'path': './pixels/duck.png',
+        'difficulty': 'hard'
+    },
+    {
+        'path': './pixels/moose.png',
+        'difficulty': 'hard'
+    },
+    {
+        'path': './pixels/rat.png',
+        'difficulty': 'hard'
+    },
+    {
+        'path': './pixels/skeleton.png',
+        'difficulty': 'hard'
+    }
+]
+
+export default data
