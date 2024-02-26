8efd2586f584ee5ec4b202c50b64ce4ed1ca1771
farsenyev <farsenyev@bk.ru>
2024-02-27 00:27:17 +0300

    fix: change API

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,8 +2,8 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        const apiUrl = process.env.API_URL;
-        const apiKey = process.env.API_KEY;
+        const apiUrl = 'https://newsapi.org/v2/';
+        const apiKey = '57e85156153d480589c1599bdea633c3';
 
         if (!apiUrl || !apiKey) {
             throw new Error('API_URL and/or API_KEY not provided in environment variables.');
