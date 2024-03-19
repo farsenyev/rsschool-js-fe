9ad15c9ac083c19407962529fd6dadecd31a4b33
farsenyev <34959976+farsenyev@users.noreply.github.com>
2024-03-19 23:01:11 +0300

    Feat/rss pz 01 create mandatory input fields (#31)
    * feat: Create Mandatory Input Fields
    
    * refactor: prittier change code
    
    * feat: done pz01
    
    * refactor: prittier change code

---
+++ src/login/login.ts
@@ -0,0 +1,63 @@
+class Login {
+  parent: HTMLElement;
+  loginContainer: HTMLDivElement | null;
+  inputName: HTMLInputElement | null;
+  inputSurname: HTMLInputElement | null;
+  submitButton!: HTMLButtonElement;
+
+  constructor(parent: HTMLElement) {
+    this.parent = parent;
+    this.loginContainer = null;
+    this.inputName = null;
+    this.inputSurname = null;
+
+    this.init();
+  }
+
+  init() {
+    this.loginContainer = document.createElement("div");
+    this.loginContainer.classList.add("login-container");
+    this.createForm();
+  }
+
+  createForm() {
+    this.inputName = document.createElement("input");
+    this.inputName.type = "text";
+    this.inputName.placeholder = "Please enter your first name";
+    this.inputName.classList.add("form-input");
+    this.inputName.required = true;
+
+    this.inputSurname = document.createElement("input");
+    this.inputSurname.type = "text";
+    this.inputSurname.placeholder = "Please enter your surname";
+    this.inputSurname.classList.add("form-input");
+    this.inputSurname.required = true;
+
+    this.submitButton = document.createElement("button");
+    this.submitButton.classList.add("button-submit");
+    this.submitButton.type = "submit";
+    this.submitButton.textContent = "Login";
+    this.submitButton.disabled = true;
+    this.submitButton.addEventListener("click", () => this.handleSubmit());
+
+    if (this.loginContainer) {
+      this.loginContainer.append(
+        this.inputName,
+        this.inputSurname,
+        this.submitButton,
+      );
+      this.parent.append(this.loginContainer);
+    }
+  }
+
+  handleSubmit() {
+    const firstName = (this.inputName as HTMLInputElement).value;
+    firstName.trim();
+    const surName = (this.inputSurname as HTMLInputElement).value;
+    surName.trim();
+
+    this.submitButton.disabled = !(firstName && surName);
+  }
+}
+
+export default Login;
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import "./style.css";
+import Login from "./login/login";
+
+const body: HTMLElement = document.body;
+const login = new Login(body); // eslint-disable-line
