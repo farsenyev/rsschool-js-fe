0f9a2787af1682fcd906a16346a87a1dd39aa30d
farsenyev <34959976+farsenyev@users.noreply.github.com>
2024-03-19 22:48:30 +0300

    Revert "Feat/rss pz 01 create mandatory input fields (#25)" (#30)
    This reverts commit 3a9688b36a0c98164647db6a0b5929831db81cbf.

--- src/app/puzzle.ts
+++
@@ -1,4 +0,0 @@
-import data from "./data";
-import Game from "./game";
-
-const game = new Game(document.body, data.rounds[0]);
--- src/main.ts
+++
@@ -1,5 +0,0 @@
-import "./style.css";
-import Login from "./login/login";
-
-const body: HTMLElement = document.body;
-const login = new Login(body); // eslint-disable-line
